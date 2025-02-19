730576543c35e9535f971165f4e7b1d5627f19f0
networkSorcerer <gigattfyhig@gmail.com>
2025-02-19 17:50:24 +0900

    데이터 분석 하기

--- ch04-1-로지스틱회귀.py
+++ ch04-1-로지스틱회귀.py
@@ -32,3 +32,38 @@ print(kn.predict(test_scaled[:5]))
 import numpy as np
 proba = kn.predict_proba(test_scaled[:5])
 print(np.round(proba, decimals=4))
+
+distances, indexes = kn.kneighbors(test_scaled[3:4])
+print(train_target[indexes])
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+z = np.arange(-5,5,0.1)
+phi = 1 / (1 + np.exp(-z))
+plt.plot(z,phi)
+plt.xlabel('z')
+plt.ylabel('phi')
+plt.show()
+
+char_arr = np.array(['A','B','C','D','E'])
+print(char_arr[[True, False, True, False, False]])
+
+bream_smelt_indexes = (train_target == 'Bream') | (train_target == 'Smelt')
+train_bream_smelt = train_scaled[bream_smelt_indexes]
+target_bream_smelt = train_target[bream_smelt_indexes]
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr.fit(train_bream_smelt, target_bream_smelt)
+
+print(lr.predict(train_bream_smelt[:5]))
+
+print(lr.predict_proba(train_bream_smelt[:5]))
+
+print(lr.classes_)
+
+# 로지스틱 ? 뉴스에서 어느 해당 기업을 검색 했을때 GOOD NEWS , BAD NEWS 나눌떄 써볼? 이진분류 
+# 다항은 단위 값이 다른 데이터를 가지고 학습 할때
+# 근접은 기업이 요구하는 기술 스택 + 회원의 기술 스택을 통해서 매칭 할때?
+
